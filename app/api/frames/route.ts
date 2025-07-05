e7eaa94bd2c7f28c813c9031670744add0de0250
Kingvinu7 <88230429+Kingvinu7@users.noreply.github.com>
2025-07-05 13:08:54 +0530

    Update route.ts

--- app/api/frames/route.ts
+++ app/api/frames/route.ts
@@ -7,11 +7,11 @@ const handler = frames(async (ctx) => {
 
   return {
     image: (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%', backgroundColor: '#FFD700', fontSize: 60, color: 'navy' }}>
-          <h1>Frames.js Clicker</h1>
-          <p>Clicks: {newCount}</p>
-        </div>
-      ),
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%', backgroundColor: '#FFD700', fontSize: 60, color: 'navy' }}>
+            <h1>Frames.js Clicker</h1>
+            <p>Clicks: {newCount}</p>
+          </div>
+        ),
     buttons: [
       {
         label: `Click Me!`,
