e2a1b3d9b8ac85767488d5638bdfa969e856e9d3
Kingvinu7 <88230429+Kingvinu7@users.noreply.github.com>
2025-07-07 17:59:17 +0530

    Create head.tsx

---
+++ app/miniapp/head.tsx
@@ -0,0 +1,20 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="fc:miniapp"
+        content='{
+          "version": "next",
+          "imageUrl": "https://fc-taps.vercel.app/og.png",
+          "button": {
+            "title": "Play Tap Game",
+            "action": {
+              "type": "launch_miniapp",
+              "url": "https://fc-taps.vercel.app/miniapp"
+            }
+          }
+        }'
+      />
+    </>
+  );
+}
